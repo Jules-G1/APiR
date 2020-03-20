bf1ecacd46605f9022d578d853973b9a52b08649
Jules <57092931+Jules-G1@users.noreply.github.com>
2020-03-20 15:09:53 +0100

    Update app2.R

--- app2.R
+++ app2.R
@@ -1,6 +1,10 @@
 library(shiny)
 library(tidyverse)
+library(ggplot2)
+library(cofeatureR)
+library(plotly)
 
+mydata <- read.csv("co2_temperature_pop.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -8,24 +12,23 @@ ui <- fluidPage(
     # Application title
     titlePanel("Emission of CO2 vs Temperature variations"),
     
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             checkboxGroupInput("region",
                                "Select a Region",
                                choices=levels(mydata$region_x),selected = levels(mydata$region_x)),
-            #br(),
+            br(),
             sliderInput("year",
                         "Select a Year",
                         min = 1990,
                         max = 2010,
-                        value = 2000, animate=T)
+                        value = 1990, animate=T)
         ),
         
-        # Show a plot of the generated distribution
+
         mainPanel(
             tabsetPanel(type = "tabs",
-                        tabPanel("Plot", plotOutput("distPlot")),
+                        tabPanel("Plot", plotlyOutput("distPlot")),
                         tabPanel("Map", verbatimTextOutput("map")),
                         tabPanel("Table", tableOutput("table"))
             )
@@ -40,15 +43,20 @@ mydata <- read.csv("co2_temperature_pop.csv")
 server <- function(input, output) {
     
     
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        df <- mydata %>% select(region_x,annee,emissions_totale,variation_temperature,population_totale) %>% filter(region_x %in% input$region, annee == input$year) %>% group_by(region_x)
-        x <- df$variation_temperature
-        y <- df$emissions_totale
+    output$distPlot <- renderPlotly({
+        mydata_fit <- mydata %>% select(region_x, continent, annee, emissions_totale, variation_temperature, population_totale) %>% filter(region_x %in% input$region, annee == input$year) %>% group_by(region_x)
+        x <- mydata_fit$variation_temperature
+        y <- mydata_fit$emissions_totale
         
-        # draw the histogram with the specified number of bins
-        plot(x, y,ylab="Intensité des émissions",
-             xlab="Variation de temperature", xlim=c(-1.5050 , 2.5350),ylim=c(-3236,12529194))
+        plot_ly(mydata_fit, x=x, y=y, type='scatter', mode='markers', size= ~population_totale, sizes = c(10, 80), color= ~continent, marker = list(symbol = 'circle', sizemode = 'diameter', line = list(width = 2, color = '#FFFFFF')), text = ~region_x, hovertemplate = paste(
+            "<b>%{text}</b><br><br>",
+            "%{yaxis.title.text}: %{y:,.0f}<br>",
+            "%{xaxis.title.text}: %{x:.2f}°C<br>",
+            "Population: %{population_totale}",
+            "<extra></extra>"
+        )) %>% layout(
+            xaxis = list(title = "Variation température",range = c(-1.6050, 2.5450)),
+            yaxis = list(title = "Emissions totale",range = c(-4336,13529294)))
     })
 }
 
