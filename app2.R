154c683a7b3d4445a4507336be546abc53a35950
Jules <57092931+Jules-G1@users.noreply.github.com>
2020-03-28 16:21:22 +0100

    Update app2.R

--- app2.R
+++ app2.R
@@ -1,12 +1,9 @@
 library(shiny)
 library(tidyverse)
-library(ggplot2)
-library(cofeatureR)
 library(plotly)
 library(leaflet)
-
-mydata <- read.csv("co2_temperature_pop.csv", header = T, sep=";", dec=".")
-
+library(DT)
+mydata <- read.csv("co2_temperature_pop.csv", header = T, sep=",", dec=".")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     
@@ -17,7 +14,7 @@ ui <- fluidPage(
         sidebarPanel(
             checkboxGroupInput("region",
                                "Select a Region",
-                               choices=levels(mydata$region_x),selected = levels(mydata$region_x)),
+                               choices=levels(mydata$region),selected = levels(mydata$region)),
             br(),
             sliderInput("year",
                         "Select a Year",
@@ -26,12 +23,12 @@ ui <- fluidPage(
                         value = 1990, animate=T)
         ),
         
-
+        
         mainPanel(
             tabsetPanel(type = "tabs",
-                        tabPanel("Plot", plotlyOutput("distPlot")),
-                        tabPanel("Map", leafletOutput("map")),
-                        tabPanel("Table", tableOutput("table"))
+                        tabPanel("Plot", br(),plotlyOutput("distPlot")),
+                        tabPanel("Map", br(),leafletOutput("map")),
+                        tabPanel("Table", br(),dataTableOutput("table"))
             )
         )
     )
@@ -39,38 +36,86 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 
-mydata <- read.csv("co2_temperature_pop.csv", header = T, sep=";", dec=".")
+
 
 server <- function(input, output) {
     
-    
     output$distPlot <- renderPlotly({
-        mydata_fit <- mydata %>% select(region_x, continent, annee, emissions_totale, variation_temperature, population_totale) %>% filter(region_x %in% input$region, annee == input$year) %>% group_by(region_x)
+        mydata_fit <- mydata %>%
+            select(region, continent,
+                   annee, emissions_totale,
+                   variation_temperature, population_totale) %>%
+            filter(region %in% input$region, annee == input$year) %>%
+            group_by(region)
         x <- mydata_fit$variation_temperature
         y <- mydata_fit$emissions_totale
-        pop <- mydata_fit$population_totale
         
-        plot_ly(mydata_fit, x=x, y=y, type='scatter', mode='markers', color= ~continent, marker = list(symbol = 'circle', size= ~population_totale, sizeref=2 * max(pop)/10000,  sizemode = 'area',sizemin=10, sizemax=110, line = list(width = 2, color = '#FFFFFF')), text = ~region_x, hovertemplate = paste(
-            "<b>%{text}</b><br><br>",
-            "%{yaxis.title.text}: %{y:,.0f}<br>",
-            "%{xaxis.title.text}: %{x:.2f}°C<br>",
-            "Population: %{marker.size:,}",
-            "<extra></extra>"
-        )) %>% layout(
-            xaxis = list(title = "Variation température",range = c(-1.6050, 2.5450)),
-            yaxis = list(title = "Emissions totale",range = c(-4336,13529294)))
+        mydata_fit$size <- sqrt(mydata_fit$population_totale * 0.000009)
+        
+        fig <- plot_ly(mydata_fit, x = ~variation_temperature,
+                       y = ~emissions_totale, color = ~continent,
+                       size = ~size,
+                       type = 'scatter', mode = 'markers', sizes = c(min(mydata_fit$size), max(mydata_fit$size)),
+                       marker = list(symbol = 'circle', sizemode = 'diameter',
+                                     line = list(width = 2, color = '#FFFFFF')),
+                       text = ~paste('Region:', region,
+                                     '<br>Emissions:', emissions_totale,
+                                     '<br>Variation de température:', variation_temperature,'°C',
+                                     '<br>Pop.:', population_totale))
+        fig <- fig %>% layout(xaxis = list(title = 'Variation de temperature (en °C)',
+                                           gridcolor = 'rgb(255, 255, 255)',
+                                           range = c(-1.6050, 2.5450),
+                                           zerolinewidth = 1,
+                                           ticklen = 5,
+                                           gridwidth = 2),
+                              yaxis = list(title = 'Emissions de C02',
+                                           gridcolor = 'rgb(255, 255, 255)',
+                                           range = c(-1500000,13529294),
+                                           zerolinewidth = 1,
+                                           ticklen = 5,
+                                           gridwith = 2))
+        
+        
+        fig
+    })
+    
+    output$table <- renderDataTable({
+        datatable(mydata %>%
+                      select(region, continent,
+                             annee, emissions_totale,
+                             variation_temperature, population_totale) %>%
+                      filter(region %in% input$region, annee == input$year) %>%
+                      group_by(region) %>%
+                      arrange(desc(emissions_totale), emissions_totale),
+                  options = list(
+                        lengthMenu = list(c(10, 20, -1), c('10', '20', 'All')),
+                        pageLength = 10
+        )) 
+        
     })
     
     output$map <- renderLeaflet({
-        mydata_fit = mydata %>% filter(annee== input$year)
+        mydata_fit = mydata %>%
+            select(region, lng,
+                   lat, continent,
+                   annee, emissions_totale,
+                   variation_temperature, population_totale) %>%
+            filter(region %in% input$region, annee == input$year) %>%
+            group_by(region)
+        
         couleurs <- colorNumeric("YlOrRd", mydata_fit$variation_temperature, n=22)
-        mymap90 = leaflet(mydata_fit)%>% addTiles() %>%
+        
+        mymap90 = leaflet(mydata_fit) %>%
+            addTiles() %>%
             addCircleMarkers(lng = mydata_fit$lng, 
                              lat = mydata_fit$lat, 
                              fillOpacity = 10,
-                             popup = ~paste("Variation de la température pour la région",mydata_fit$region_x, "pour l'année",mydata_fit$annee, ":", mydata_fit$variation_temperature),
+                             popup = ~paste("Variation de la température pour la région",mydata_fit$region,
+                                            "pour l'année",mydata_fit$annee, ":", mydata_fit$variation_temperature),
                              color= ~couleurs(variation_temperature)) %>% 
-            addLegend(pal = couleurs, values = ~variation_temperature, opacity = 0.9)
+            addLegend(pal = couleurs,
+                      values = ~variation_temperature,
+                      opacity = 0.9)
     })
 }
 
